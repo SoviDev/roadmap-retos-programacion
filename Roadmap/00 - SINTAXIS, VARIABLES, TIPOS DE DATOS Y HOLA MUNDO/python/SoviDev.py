1de4acbcd95446c92a07cbfa4492e284ee36b7af
SoviDev <168366068+SoviDev@users.noreply.github.com>
2024-04-28 21:01:20 -0600

    Create SoviDev.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/SoviDev.py
@@ -0,0 +1,30 @@
+# https://www.python.org/
+
+# Comentario de una linea
+
+"""
+Los comentarios en varias lineas se realizan con las 3 comillas dobles 
+se cierra de la misma forma 
+"""
+
+'''
+Tambien se puede realizar con
+3 comillas simples
+
+'''
+
+mi_variable = "Hola"
+
+## En Python no hay constantes sin embargo el utilizar todo en mayusculas quiere decir que quieres poner una constante y que nadie lo debe de modificar
+MI_CONSTANTE = "Mi Constanate"
+
+#En python no se tiene que declarar el tipo de dato y puede cambiar en tiempo de ejecucion.
+
+mi_integer = 1
+mi_float = 1.5
+mi_bool = True
+mi_string "Hola Mundo!"
+
+
+print("Hola, Python!!")
+
